eff0d0b02cdc22bf4fcb9b052c575a1757b0e51c
mohitj27 <mjain8156@gmail.com>
2017-11-21 12:19:37 +0530

    change in js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function submitAnswers(){
 	var total =5;
 	var score =0;
 	
-var q1 =document.forms["quizForm]["q1"].value;
-var q2 =document.forms["quizForm]["q2"].value;
-var q3 =document.forms["quizForm]["q3"].value;
-var q4 =document.forms["quizForm]["q4"].value;
-var q5 =document.forms["quizForm]["q5"].value;
+var q1 =document.forms["quizForm"]["q1"].value;
+var q2 =document.forms["quizForm"]["q2"].value;
+var q3 =document.forms["quizForm"]["q3"].value;
+var q4 =document.forms["quizForm"]["q4"].value;
+var q5 =document.forms["quizForm"]["q5"].value;
 
 
   for(i=1;i<=total;i++){
